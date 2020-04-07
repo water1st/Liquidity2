f1d7cff5627256201a573747b9cb29e388c4f973
何梓杰 <hezijie@glbdata.com>
2020-04-07 15:41:25 +0800

    添加blocker

---
+++ src/Liquidity2.Extensions.Blocker.SampleTaskBlocker/Blocker.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace Liquidity2.Extensions.Blocker.SampleTaskBlocker
+{
+    internal class Blocker : IBlocker
+    {
+        public void Block(Task task)
+        {
+            Task.WaitAll(task);
+        }
+
+        public TResult Block<TResult>(Task<TResult> task)
+        {
+            TResult result = default;
+
+            var t = new Task(async () =>
+            {
+                result = await task;
+            });
+
+            t.Start();
+            Task.WaitAll(t);
+
+            return result;
+        }
+    }
+}
