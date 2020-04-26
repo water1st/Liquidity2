192ce3b659b843c599bf2bfefffea9e4029d9153
何梓杰 <hezijie@glbdata.com>
2020-04-26 21:11:59 +0800

    自动创建sqlite

--- src/Liquidity2.Extensions.Data.LocalStorage/Factories/DbConnectionFactory.cs
+++ src/Liquidity2.Extensions.Data.LocalStorage/Factories/DbConnectionFactory.cs
@@ -1,5 +1,7 @@
-﻿using System.Data;
+﻿using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
+using System.IO;
 
 namespace Liquidity2.Extensions.Data.LocalStorage
 {
@@ -16,12 +18,37 @@ namespace Liquidity2.Extensions.Data.LocalStorage
         {
             try
             {
+                var args = AnalyzingConnectionString(options.ConnectionString);
+                var filePath = args["data source"];
+                var dirPath = Path.GetDirectoryName(filePath);
+
+                if (!Directory.Exists(dirPath))
+                    Directory.CreateDirectory(dirPath);
+
+                if (!File.Exists(filePath))
+                    SQLiteConnection.CreateFile(filePath);
+
                 return new SQLiteConnection(options.ConnectionString);
             }
             catch
             {
                 throw;
             }
+        }
+
+        private IDictionary<string, string> AnalyzingConnectionString(string connectionString)
+        {
+            var result = new Dictionary<string, string>();
+            var args = connectionString.Split(';');
+            foreach (var arg in args)
+            {
+                var datas = arg.Split('=');
+                var key = datas[0].Trim().ToLower();
+                var value = datas[1].Trim();
+                result.Add(key, value);
+            }
+
+            return result;
         }
     }
 }
